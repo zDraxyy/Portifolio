0f1ae5bcff8a3649b743c6447ed041a345719a56
Draxy <ludhi2ram@gmail.com>
2025-06-04 02:26:37 +0000

    add coisas na pagina principal

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+simport Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
