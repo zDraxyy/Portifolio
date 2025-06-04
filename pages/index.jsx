e3d9293a9d467cdba2463ae7dad34690434379bc
Draxy <ludhi2ram@gmail.com>
2025-06-04 02:00:13 +0000

    atualizaçao da pagina principal

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,9 +24,55 @@ export default function Home() {
           zDraxy
         </h1>
         <p>
-            òla, seja bem vindo ao meu portifolio,aqui você sabera mais sobre mim.
+            òla, seja bem vindo ao meu portifolio, aqui você vai saber mais sobre mim.
         </p> 
         </main>
+        <section>
+         <h2> Sobre mim:</h2>
+         <p>
+          Tenho 16 anos, sou um desenvolvedor full stack,<br/>
+          procuro sempre aprender mais ainda sobre programação.<br/>
+          Faço alguns hobbys como fazer addons e videos no youtube.
+         </p>
+        </section>
+        <section>
+          <a href="projetos/projects.jsx">
+          <h2>
+            Projeytos:
+            </h2>
+            <p>
+              Meus sites e projetos que estão feitos <br/>
+               ou em desenvolviemnto
+            </p>
+            </a>
+        </section>
+        <section>
+          <h2>
+            Addons/Mods:
+          </h2>
+          <p>
+            Addons e Mods para Minecraft
+          </p>
+        </section>
+        <div>
+          <h2>
+            Tecnologias:
+          </h2>
+        </div>
+        <div>
+          <h2>
+            Contatos
+          </h2>
+          <p>
+            Email: draxypersonalcontact@gmail.com
+          </p>
+        </div>
+        <footer>
+          <h2>
+            Redes Sociais:
+          </h2>
+        </footer>
         </>
+
   );
 }
