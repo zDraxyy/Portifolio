f3cc46459041e77144a969be89f6bdc4d5043945
Draxy <ludhi2ram@gmail.com>
2025-06-06 21:08:05 +0000

    algo ai

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,10 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+import Head from "next/head";
 export default function Home() {
   return (
         <>
-      <Head>
-    <title>Portifolio</title>
-    </Head>
-      <main id="main">
+      <main className={styles.main}>
         <h1>
           zDraxy
         </h1>
